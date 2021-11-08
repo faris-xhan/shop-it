c040e97549064c6b59d00678bc726982f997dc69
faris <faris@mail.com>
2021-11-08 09:28:08 -0800

    mui: start work on featured products :construction:

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { Grid } from "@mui/material";
 import Header from "./components/Header/Header";
 import React from "react";
 import Categories from "./components/Categories/Categories";
+import FeaturedProducts from "./components/FeaturedProducts";
 
 function App() {
   return (
     <Grid>
       <Header />
+      <FeaturedProducts />
       <Categories />
     </Grid>
   );
---
+++ src/components/FeaturedProducts.jsx
@@ -0,0 +1,11 @@
+import { Grid } from "@mui/material";
+
+const FeaturedProducts = (props) => {
+  return (
+    <Grid>
+      <h1>Featured Products</h1>
+    </Grid>
+  );
+};
+
+export default FeaturedProducts;
