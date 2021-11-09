cbe9ffb12b4d5e5042c11667233d8c9f7eea3eee
faris <faris@mail.com>
2021-11-08 22:35:12 -0800

    mock: add featured_produts.js :clown_face:

---
+++ src/__mock__/featured_products.js
@@ -0,0 +1,35 @@
+export const featuredProducts = [
+  {
+    id: 1,
+    title: "Oxford Duo BagBrown",
+    mediaSrc: "/images/product.jpg",
+    current_price: "Rs.2000",
+    review:
+      "I have received the Oxford duo bag I love it its Beautiful and thank you for your amazing Customer serviceI have received the Oxford duo bag I love it its Beautiful and thank you for your amazing Customer serviceI have received the Oxford",
+    totalReviews: 5,
+    rating: 4.5,
+    href: "#",
+  },
+  {
+    id: 2,
+    title: "Mini Cross Body Bag",
+    mediaSrc: "/images/product.jpg",
+    current_price: "Rs.2000",
+    review:
+      " amazing Customer serviceI have received the Oxford duo bag I love it its Beautiful and thank you for your amazing Customer serviceI have received the Oxford",
+    totalReviews: 5,
+    rating: 5,
+    href: "#",
+  },
+  {
+    id: 3,
+    title: "Capri Handbag Peach",
+    mediaSrc: "/images/product.jpg",
+    current_price: "Rs.2000",
+    review:
+      "I have received the Oxford duo bag I love it its Beautiful and thank you for your amazing Customer serviceI have received the Oxford duo bag I love it.",
+    totalReviews: 5,
+    rating: 3.5,
+    href: "#",
+  },
+];
