50df7e6a932cc36c1188b6207c0550840cfdc9ab
faris <faris@mail.com>
2021-11-09 06:06:32 -0800

    redux: add shoppingCart reducer to store :sparkles:

--- src/store.js
+++ src/store.js
@@ -1,6 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
+import shoppingCartReducer from "./features/shoppingCart/shoppingCartSlice";
 
 export const store = configureStore({
   reducer: {
+    shoppingCart: shoppingCartReducer,
   },
 });
