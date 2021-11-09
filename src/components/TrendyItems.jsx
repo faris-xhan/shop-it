c0dc2210d501f02684a722e3b300dcb4265c0ce8
faris <faris@mail.com>
2021-11-08 23:44:51 -0800

    fix: fix the eslint warning :rotating_light:

--- src/components/TrendyItems.jsx
+++ src/components/TrendyItems.jsx
@@ -1,9 +1,10 @@
-import { Grid, Stack, Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import { useState } from "react";
 import { products as dummyProduct } from "../__mock__/products";
 import ProductCard from "./ProductCard";
 
 const TrendyItems = (props) => {
+  // eslint-disable-next-line
   const [products, setProducts] = useState(dummyProduct);
   return (
     <Grid sx={{ p: 1 }}>
