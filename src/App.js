5185c346163f61ed50ffb57d8b29d064a6167dec
faris <faris@mail.com>
2021-11-10 00:03:09 -0800

    refactor: add routes :recycle:

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import { Divider, Grid } from "@mui/material";
 import Header from "./components/Header/Header";
 import React from "react";
-import Categories from "./components/Categories/Categories";
-import FeaturedProducts from "./components/FeaturedProducts";
-import TrendyItems from "./components/TrendyItems";
+import HomeRoute from "./routes/Home.route";
 import Footer from "./components/Footer/Footer";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Grid>
       <Header />
-      <FeaturedProducts />
-      <Divider />
-      <Categories />
-      <TrendyItems />
+      <Routes>
+        <Route path="/" element={<HomeRoute />} />
+      </Routes>
       <Divider />
       <Footer />
     </Grid>
