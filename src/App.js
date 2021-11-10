692859fe323a10f2a4707addebfef14a27563eea
faris <faris@mail.com>
2021-11-10 00:06:56 -0800

    route: add not found route :goal_net:

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { Divider, Grid } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import React from "react";
 import HomeRoute from "./routes/Home.route";
 import Footer from "./components/Footer/Footer";
-import { Routes, Route } from "react-router-dom";
+import NotFoundRoute from "./routes/NotFound.route";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<HomeRoute />} />
+        <Route path="*" element={<NotFoundRoute />} />
       </Routes>
       <Divider />
       <Footer />
---
+++ src/routes/NotFound.route.jsx
@@ -0,0 +1,12 @@
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+
+const NotFoundRoute = (props) => {
+  return (
+    <Grid>
+      <Typography variant="h1">404 Not Found</Typography>
+    </Grid>
+  );
+};
+
+export default NotFoundRoute;
